e5540aa1f03793eb1202a74a5553ef277112437a
upwork200526 <upwork200526@gmail.com>
2021-04-05 03:15:14 +0530

    SubmitForApproval.js modified for default approver and profile name, email cols

--- force-app/main/default/lwc/submitForApproval/submitForApproval.js
+++ force-app/main/default/lwc/submitForApproval/submitForApproval.js
@@ -1,10 +1,16 @@
-import { LightningElement, track, api } from 'lwc';
+import { LightningElement, track, api, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import submitApprovalProcess from '@salesforce/apex/MultiUserApprovalController.submitApprovalProcess';
+import OWNER from '@salesforce/schema/Custom_Product_Code__c.CreatedBy.Name'
+import PROFILENAME from '@salesforce/schema/Custom_Product_Code__c.CreatedBy.Profile.Name'
+import EMAIL from '@salesforce/schema/Custom_Product_Code__c.CreatedBy.Email'
+import { getRecord } from 'lightning/uiRecordApi';
 
 //Headers for Approvers Table
 const columns = [
     { label: 'Approver Name', fieldName: 'Name' },
+    { label: 'Email', fieldName: 'Email' },
+    { label: 'Profile Name', fieldName: 'profileName' },
     {
         label: 'Actions',
         type: 'button',
@@ -20,22 +26,43 @@ const columns = [
     }
 ];
 //String query to search fields in User Search Lookup
-const searchFields = "Id, Name";
+const searchFields = "Id, Name, Email, Profile.Name";
 
 export default class SubmitForApproval extends LightningElement {
 
     searchFields = searchFields;
     columns = columns;
-
+    @api recordId; //Salesforce Id of the record for which approval is to be submitted
     @track selectedRecordIds = [];
     @track data = [];
-    
-    @api recordId; //Salesforce Id of the record for which approval is to be submitted
 
+    @track var1 =[];
+    @wire (getRecord,{recordId:'$recordId',fields:[OWNER,PROFILENAME,EMAIL]})
+    getRecordCallBack({data,error}) {
+        if(data){
+            let approver1 = {
+                Id: data.fields.CreatedBy.value.Id,
+                Name: data.fields.CreatedBy.value.fields.Name.value,
+                Email: data.fields.CreatedBy.value.fields.Email.value,
+                profileName: data.fields.CreatedBy.value.fields.Profile.displayValue
+            }
+            this.data = [...this.data, approver1];
+            this.var1 = [...this.var1, approver1];
+            console.log(data);
+            console.log("data block");
+        }
+        if(error){
+            console.log(error);
+        }
+    }
+  
+    
     //Method called when user selectes any approver from search lookup
     handleApproverSearch(event) {
         let approver = event.detail;
+        approver.profileName = approver.Profile?approver.Profile.Name:'';
         this.data = [...this.data, approver];
+        console.log(approver);
         this.selectedRecordIds.push(approver.Id);
     }
 
